0bfe51917db478ea94c0089387d359752eefddb3
shahriar350 <shahriar350@gmail.com>
2021-08-17 02:17:03 +0600

    first version

---
+++ pages/index.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="row flex-md-row flex-column-reverse">
+          <div class="col-lg-9 col-md-8 col-sm-7 col-12">
+            <h3 class="text-center">Saifullah shahen</h3>
+            <b-card>
+              <b-card-title>Profile</b-card-title>
+              <b-card-text>
+                I am Saifullah Shahen. I completed my BSc in Computer Science and Engineering. I completed many project
+                in university related c, c++, php, python, assembly, database and so more.
+                I am confident and word worker. I can learn any programming related task faster. You can be found me
+                polite. <br/>
+                Now i am confident about <em>full stack web development</em>. Already i completed some ecommerce website with restapi, chatapp and some small projects. I have started my web development with Laravel, Django, Javascript, Vue, Bootstrap, Tailwind.
+                But as a fresher i can move any technology quickly. I am concern about security and faster app and confident about tasks.
+              </b-card-text>
+            </b-card>
+            <b-card class="mt-3">
+              <b-card-title>Contact</b-card-title>
+              <b-card-text>
+                <div class="d-flex align-content-center">
+                  <div class="pr-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         class="bi bi-phone" viewBox="0 0 16 16">
+                      <path
+                        d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+                      <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                    </svg>
+                  </div>
+                  <p>+880 1752495467</p>
+                </div>
+                <div class="d-flex align-content-center">
+                  <div class="pr-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         class="bi bi-envelope" viewBox="0 0 16 16">
+                      <path
+                        d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+                    </svg>
+                  </div>
+                  <a href="mailto:shahriar350@gmail.com">shahriar350@gmail.com</a>
+                </div>
+
+              </b-card-text>
+            </b-card>
+          </div>
+          <div class="col-lg-3 col-md-4 col-sm-5 col-12 my-3">
+            <div class="row justify-content-center">
+              <div class="col-md-12 col-6">
+                <img src="~static/self.jpg" alt="saifullah shahen" width="100%">
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <b-card title="Specialization" class="mt-3">
+          <!--          <b-card-header>Skill</b-card-header>-->
+          <div class="d-flex">
+            <p class="font-weight-bold mr-3">Language: </p>
+            <p class="px-3">Python, MySQL, HTML, Javascript</p>
+          </div>
+          <div class="d-flex">
+            <p class="font-weight-bold mr-3">Framework: </p>
+            <p class="px-3">Laravel, Django, Micro-services, Vue, Nuxt</p>
+          </div>
+
+
+        </b-card>
+        <b-card title="Intermediate Skill" class="mt-1">
+          <!--          <b-card-header>Skill</b-card-header>-->
+          <div class="d-flex">
+            <p class="font-weight-bold mr-3">Language: </p>
+            <p class="px-3">C, C++, Postgres</p>
+          </div>
+          <div class="d-flex">
+            <p class="font-weight-bold mr-3">Framework: </p>
+            <p class="px-3">Angular</p>
+          </div>
+          <div class="d-flex">
+            <p class="font-weight-bold mr-3">Python for data science: </p>
+            <p class="px-3">Numpy, Pandas, Matplolib, Seaborn, Spacy, Scikit-learn</p>
+          </div>
+
+
+        </b-card>
+        <b-card title="Education" class="mt-3">
+          <b-card-text>
+            <b-table striped hover :items="education"></b-table>
+          </b-card-text>
+
+        </b-card>
+
+        <b-card title="Career path" class="mt-3">
+          <b-card-text>
+            I am self-motivated student. I started my programming journey at the first semester with PHP and Laravel at <em>CreativeIt institute</em>.
+            At that time i learn about web and completed some project which was approved by my teacher. Later i have learned Python and Django. In the mean time i learn OOP and database which is helped me to learn web development more efficiently.
+            Later i interest to coding Data science and implemented some Machine learning algorithms. In future i will work for Machine learning objectives.
+          </b-card-text>
+        </b-card>
+        <b-card title="Publications" class="mt-3">
+          <b-card-text>
+            <a href="https://ieeexplore.ieee.org/abstract/document/9322814">Human Abnormality Classification Using Combined CNN-RNN Approach
+            </a>
+          </b-card-text>
+        </b-card>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      education: [
+        {
+          Type: 'SSC',
+          sector: 'Science',
+          school: 'Sardah Govt Pilot High School',
+          grade: '4.69'
+        },
+        {
+          Type: 'HSC',
+          sector: 'Science',
+          school: 'Sardah Govt College',
+          grade: '4.35'
+        },
+        {
+          Type: 'Bachelor of Science',
+          sector: 'Computer Science and Engineering',
+          school: 'Bangladesh University of Business and Technology',
+          grade: '3.52'
+        },
+
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  padding: 0 !important;
+  margin: 0 !important;
+}
+</style>
